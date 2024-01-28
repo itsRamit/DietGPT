c308dae459102bd37d89e9f2f28172a2255103a5
ItsRamit <ece22145@iiitkalyani.ac.in>
2024-01-28 18:21:18 +0530

    form active through buttons

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const API_URL = "https://api.openai.com/v1/chat/completions";
 const promptInput = document.getElementById("userInput");
 const sendBtn = document.getElementById("submit-button");
 const resultText = document.querySelector(".output");
+const dietForm = document.getElementById("Dietform");
+const dishNameForm = document.getElementById("DishNameform");
+const ingredentsFormContainer = document.getElementById("Ingredentsform");
+
+function DietInputForm() {
+  dietForm.style.display = "flex";
+  dishNameForm.style.display = "none";
+  ingredentsFormContainer.style.display = "none";
+}
+
+function GenerateRecipeForm() {
+  dietForm.style.display = "none";
+  dishNameForm.style.display = "flex";
+  ingredentsFormContainer.style.display = "none";
+}
+
+function GenerateDishForm() {
+  dietForm.style.display = "none";
+  dishNameForm.style.display = "none";
+  ingredentsFormContainer.style.display = "flex";
+}
 
 async function generate() {
   try {
@@ -22,4 +43,15 @@ async function generate() {
     console.log(data.choices[0].message.content);
     resultText.innerText = data.choices[0].message.content;
   } catch (e) {}
+}
+
+function addNewInput() {
+  const newInput = document.createElement("input");
+  newInput.type = "text";
+  newInput.id = "Ingredents";
+  newInput.name = "Ingredents";
+
+  // Append the new input element to the target div
+  const targetDiv = document.getElementById("IngredentsFormBox");
+  targetDiv.appendChild(newInput);
 }
