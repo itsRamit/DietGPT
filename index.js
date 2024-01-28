a642a024f3174ffa38e077fb612de5ec4ca38ab1
ItsRamit <ece22145@iiitkalyani.ac.in>
2024-01-28 17:07:23 +0530

    integrated open AI 3.5

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function addNewInput() {
-    // Create a new input element
-    var newInput = document.createElement('input');
-    newInput.type = 'text';
-    newInput.name = 'dynamicInput'; // You can set a unique name for each input if needed
+let API_KEY = "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";
+const API_URL = "https://api.openai.com/v1/chat/completions";
 
-    // Append the new div to the form
-    document.getElementById('dynamicForm').insertBefore(newInput, document.getElementById('submitbutton'));
+const promptInput = document.getElementById("userInput");
+const sendBtn = document.getElementById("submit-button");
+const resultText = document.querySelector(".output");
+
+async function generate() {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + API_KEY,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: promptInput.value }],
+      }),
+    });
+    const data = await response.json();
+    console.log(data.choices[0].message.content);
+    resultText.innerText = data.choices[0].message.content;
+  } catch (e) {}
 }
